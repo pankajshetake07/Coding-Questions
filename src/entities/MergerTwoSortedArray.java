2df62e60f653d6cabfe06280325ddaebba25b77f
pankaj shetake <shetakepankaj2002@gmail.com>
2025-01-28 10:14:45 +0530

    Solved LeetCode problem number 88 Merge Sorted Array

---
+++ src/entities/MergerTwoSortedArray.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class MergerTwoSortedArray {
+
+    static void merge(int[] nums1, int m, int[] nums2, int n){
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        while(j>=0){
+            if(i>=0 && nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
+            }
+            else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+    public static void main(String[] args){
+        int [] nums1 = {1,2,3,0,0,0};
+        int [] nums2 = {4,5,6};
+        int m = nums1.length;
+        int n = nums2.length;
+        merge(nums1, m, nums2, n);
+        for(int i=0; i<m; i++){
+            System.out.print(nums1[i]+" ");
+        }
+    }
+}
